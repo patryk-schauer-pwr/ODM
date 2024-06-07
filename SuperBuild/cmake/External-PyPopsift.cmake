94e05f23557b627cf758df8f7be6df233c7e3d2b
Patryk Schauer <patryk@schauer.com.pl>
2024-06-07 22:01:23 +0200

    change to pwr git

--- SuperBuild/cmake/External-PyPopsift.cmake
+++ SuperBuild/cmake/External-PyPopsift.cmake
@@ -11,8 +11,8 @@ if(CUDA_FOUND)
         STAMP_DIR         ${_SB_BINARY_DIR}/stamp
         #--Download step--------------
         DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
-        GIT_REPOSITORY    https://github.com/OpenDroneMap/pypopsift
-        GIT_TAG           288
+        GIT_REPOSITORY    https://github.com/patryk-schauer-pwr/ODM
+        #GIT_TAG           288
         #--Update/Patch step----------
         UPDATE_COMMAND    ""
         #--Configure step-------------
